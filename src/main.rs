082d0577b17e2b8cfc7324ed9e9df7d530f48f90
Ruzafan <marcramiro@gmail.com>
2020-06-29 16:07:52 +0200

    change rocket for wrap

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
-use rustlang_rocket_mongodb::rocket;
-fn main() {
-    rocket().launch();
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello = warp::path!("hello" / String)
+        .map(|name| format!("Hello, {}!", name));
+
+    warp::serve(hello)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
