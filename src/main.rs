423e00d02cf896b3b8741f80b06e81286e30a314
Ariel Amor <it@inbetly.com>
2020-06-30 09:11:45 +0200

    problems

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use warp::Filter;
-mod MongoRepository;
+
+mod mongo_repository;
 
 #[tokio::main]
 async fn main() {
     // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
-    
-        let get_collection = warp::get()
+    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+
+    let get_collection = warp::get()
         .and(warp::path("get_collecions"))
         .and(warp::path::end())
-        .and_then(MongoRepository::init_mongo().await);
+        .and_then(mongo_repository::init_mongo);
 
-    warp::serve(hello)
+    warp::serve(get_collection)
         .run(([127, 0, 0, 1], 3030))
         .await;
 }
