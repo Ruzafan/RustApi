26efe8cc7c177e743f781bf52a1f6a05a9ac0cb9
MRamiro <marcramiro@gmail.com>
2020-07-01 21:18:45 +0200

    delete unused

--- src/mongo_repository.rs
+++ src/mongo_repository.rs
@@ -3,12 +3,6 @@ use mongodb::error::Error;
 use warp::Rejection;
 use mongodb::bson::doc;
 
-pub async fn init_mongo() -> Result<String, Rejection> {
-    match get_databases_names().await {
-        Ok(names) => Ok(names),
-        Err(_) => Err(warp::reject()),
-    }
-}
 pub async fn init_mongo_and_insert() -> Result<String, Rejection> {
     match insert_user().await {
         Ok(names) => Ok(names),
@@ -27,15 +21,4 @@ pub async fn insert_user() -> Result<String, Error>
 
     collection.insert_one(user,None).await?;
     Ok(result)
-}
-
-async fn get_databases_names() -> Result<String, Error> {
-    let client = common::initialize_mongo().await?;
-    let mut collections = String::new();
-    // List the names of the databases in that deployment.
-    for db_name in client.list_database_names(None, None).await? {
-        collections.push_str(&db_name)
-    }
-
-    Ok(collections)
 }
