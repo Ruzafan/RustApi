d040cb3a3d9bd664b2f9c8149460f515e280fe01
Ariel Amor <it@inbetly.com>
2020-07-02 13:18:20 +0200

    solucionando tus problemas

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,25 @@
 use warp::Filter;
-use crate::User;
+
+mod User;
 mod common;
 mod mongo_repository;
-mod User;
-
 
 #[tokio::main]
-
 async fn main() {
     // GET /hello/warp => 200 OK with body "Hello, warp!"
     //let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
 
-     let add_user = warp::path("add_user")
-         .and(warp::path::param())  
-         .and(warp::path::end())
-         .and_then(|param: String| {
-            mongo_repository::insert_user(param)
-        });
+    let add_user = warp::path("add_user")
+        .and(warp::path::param())
+        .and(warp::path::end())
+        .and_then(|param: String| mongo_repository::insert_user(param));
 
     let get_user = warp::path("get_user")
-        .and(warp::path::param())  
+        .and(warp::path::param())
         .and(warp::path::end())
-        .and_then(|param: String| {
-            mongo_repository::get_user(param)
-        });
-        
+        .and_then(|param: String| mongo_repository::get_user(param));
 
     let routes = add_user.or(get_user);
 
-    warp::serve(routes)
-        .run(([127, 0, 0, 1], 3030))
-        .await;
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
--- src/mongo_repository.rs
+++ src/mongo_repository.rs
@@ -1,11 +1,11 @@
 use crate::{common, User};
-use mongodb::{bson::{self, doc}, error::Error};
-use warp::{Rejection, reply::Json};
+use mongodb::{bson::doc, bson::Document, error::Error};
+use warp::{reply::Json, Rejection};
 
 const USER_COLLECTION: &'static str = "Users";
 
 pub async fn insert_user(name: String) -> Result<String, Rejection> {
-    match insert(USER_COLLECTION,doc! { "Name": name}).await {
+    match insert(USER_COLLECTION, doc! { "Name": name}).await {
         Ok(names) => Ok(names),
         Err(_) => Err(warp::reject()),
     }
@@ -18,24 +18,23 @@ pub async fn get_user(name: String) -> Result<Json, Rejection> {
     }
 }
 
-async fn insert(collection: &str, document: mongodb::bson::Document) -> Result<String, Error>
-{
+async fn insert(collection: &str, document: mongodb::bson::Document) -> Result<String, Error> {
     let result: String = String::new();
     let client = common::initialize_mongo().await?;
     let db = client.database("blog");
     let collection = db.collection(collection);
 
-    collection.insert_one(document,None).await?;
+    collection.insert_one(document, None).await?;
     Ok(result)
 }
 
-async fn find_user(collection: &str, name: String) -> Result<User::UserData, Error>
-{
+async fn find_user(collection: &str, name: String) -> Result<Document, Error> {
     let client = common::initialize_mongo().await?;
     let db = client.database("blog");
     let collection = db.collection(collection);
     let filter = doc! { "Name": name };
-    let document = collection.find_one(filter,None).await?;
-    let mut user: User::UserData = document.unwrap();
-    Ok(user)
+
+    let mongoResult = collection.find_one(filter, None).await?;
+
+    Ok(mongoResult.unwrap())
 }
